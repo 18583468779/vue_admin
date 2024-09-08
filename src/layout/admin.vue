b24ea160ebeedd65d9fcba205de5ad193a11ebdb
xiewen <1123654054@qq.com>
2024-09-08 16:32:03 +0800

    admin 主页数据统计

--- src/layout/admin.vue
+++ src/layout/admin.vue
@@ -1,9 +1,103 @@
 <template>
-    <router-view></router-view>
+    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false"
+        @select="handleSelect">
+        <el-menu-item index="0">
+            <el-text tag="b" size="large"> HtmlCode 后台管理系统</el-text>
+        </el-menu-item>
+        <el-menu-item index="1" @click="handleChangeMenu">
+            <el-icon v-if="isCollapse">
+                <Expand />
+            </el-icon>
+            <el-icon v-else>
+                <Fold />
+            </el-icon>
+        </el-menu-item>
+        <el-menu-item index="2">
+            <el-popover placement="bottom" title="通知" :width="250" trigger="click">
+
+                <template #reference>
+                    <el-badge :value="12" :offset="[-5, 20]" class="item">
+                        <el-icon :size="30">
+                            <Message />
+                        </el-icon>
+                    </el-badge>
+                </template>
+                <div class="flex flex-wrap gap-4">
+                    <el-card style="width: 480px" shadow="hover" class="cursor-pointer">Always</el-card>
+                    <el-card style="width: 480px" shadow="hover" class="cursor-pointer">Hover</el-card>
+                    <el-card style="width: 480px" shadow="hover" class="cursor-pointer">Never</el-card>
+                </div>
+            </el-popover>
+        </el-menu-item>
+        <el-menu-item index="3">
+            <el-switch v-model="value1" :active-action-icon="Sunny" :inactive-action-icon="Moon"
+                style=" --el-switch-off-color: #333" />
+        </el-menu-item>
+        <el-sub-menu index="4">
+            <template #title>
+                <el-icon>
+                    <User />
+                </el-icon>
+                个人中心
+            </template>
+            <el-menu-item index="3-1">
+                <el-icon>
+                    <User />
+                </el-icon>
+                个人中心
+            </el-menu-item>
+            <el-menu-item index="3-2">
+                <el-icon>
+                    <Back />
+                </el-icon>
+                退出登录
+            </el-menu-item>
+        </el-sub-menu>
+    </el-menu>
+    <div class="flex gap-2">
+        <!-- 侧边栏菜单 -->
+        <sliderMenu :isCollapse="isCollapse" />
+        <router-view></router-view>
+    </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, watchEffect } from 'vue'
+import {
+    Sunny, Moon
+} from '@element-plus/icons-vue';
+import sliderMenu from '../components/menu/sliderMenu.vue';
+const isCollapse = ref(false);
+const value1 = ref(true);
+
+watchEffect(() => {
+    if (value1.value) {
+        document.documentElement.classList.add('dark')
+    } else {
+        document.documentElement.classList.remove('dark')
+    }
+})
+const handleChangeMenu = () => {
+    // 左侧菜单隐藏展开
+    isCollapse.value = !isCollapse.value;
+}
+
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+};
+
+
 
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-menu--horizontal>.el-menu-item:nth-child(2) {
+    margin-right: auto;
+}
+
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+}
+</style>
--- src/view/admin/index.vue
+++ src/view/admin/index.vue
@@ -1,100 +1,195 @@
 <template>
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false"
-        @select="handleSelect">
-        <el-menu-item index="0">
-            <el-text tag="b" size="large"> HtmlCode 后台管理系统</el-text>
-        </el-menu-item>
-        <el-menu-item index="1" @click="handleChangeMenu">
-            <el-icon v-if="isCollapse">
-                <Expand />
-            </el-icon>
-            <el-icon v-else>
-                <Fold />
-            </el-icon>
-        </el-menu-item>
-        <el-menu-item index="2">
-            <el-popover placement="bottom" title="通知" :width="250" trigger="click">
-
-                <template #reference>
-                    <el-badge :value="12" :offset="[-5, 20]" class="item">
-                        <el-icon :size="30">
-                            <Message />
+    <div class="w-full">
+        <el-row :gutter="20" class="mt-6" style="margin-left: 0;margin-right:0">
+            <el-col :span="6">
+                <div class="statistic-card ">
+                    <el-statistic value="￥98500.00">
+                        <template #title>
+                            <div style="display: inline-flex; align-items: center">
+                                总收入
+                                <el-tooltip effect="dark" content="2024年总收入" placement="top">
+                                    <el-icon style="margin-left: 4px" :size="12">
+                                        <Warning />
+                                    </el-icon>
+                                </el-tooltip>
+                            </div>
+                        </template>
+                    </el-statistic>
+                    <div class="statistic-footer">
+                        <div class="footer-item">
+                            <span>超过去年</span>
+                            <span class="green">
+                                24%
+                                <el-icon>
+                                    <CaretTop />
+                                </el-icon>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="absolute right-10 top-1/3">
+                        <el-icon size="36">
+                            <TrophyBase />
                         </el-icon>
-                    </el-badge>
-                </template>
-                <div class="flex flex-wrap gap-4">
-                    <el-card style="width: 480px" shadow="hover" class="cursor-pointer">Always</el-card>
-                    <el-card style="width: 480px" shadow="hover" class="cursor-pointer">Hover</el-card>
-                    <el-card style="width: 480px" shadow="hover" class="cursor-pointer">Never</el-card>
+                    </div>
                 </div>
-            </el-popover>
-        </el-menu-item>
-        <el-menu-item index="3">
-            <el-switch v-model="value1" :active-action-icon="Sunny" :inactive-action-icon="Moon"
-                style=" --el-switch-off-color: #333" />
-        </el-menu-item>
-        <el-sub-menu index="4">
-            <template #title>
-                <el-icon>
-                    <User />
-                </el-icon>
-                个人中心
-            </template>
-            <el-menu-item index="3-1">
-                <el-icon>
-                    <User />
-                </el-icon>
-                个人中心
-            </el-menu-item>
-            <el-menu-item index="3-2">
-                <el-icon>
-                    <Back />
-                </el-icon>
-                退出登录
-            </el-menu-item>
-        </el-sub-menu>
-    </el-menu>
-    <!-- 侧边栏菜单 -->
-    <sliderMenu :isCollapse="isCollapse" />
+
+            </el-col>
+            <el-col :span="6">
+                <div class="statistic-card">
+                    <el-statistic value="￥693700">
+                        <template #title>
+                            <div style="display: inline-flex; align-items: center">
+                                活跃用户（个）
+                                <el-tooltip effect="dark"
+                                    content="Number of users who logged into the product in one month" placement="top">
+                                    <el-icon style="margin-left: 4px" :size="12">
+                                        <Warning />
+                                    </el-icon>
+                                </el-tooltip>
+                            </div>
+                        </template>
+                    </el-statistic>
+                    <div class="statistic-footer">
+                        <div class="footer-item">
+                            <span>与上个月对比</span>
+                            <span class="red">
+                                12%
+                                <el-icon>
+                                    <CaretBottom />
+                                </el-icon>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="absolute right-10 top-1/3">
+                        <el-icon size="36">
+                            <Moon />
+                        </el-icon>
+                    </div>
+                </div>
+            </el-col>
+            <el-col :span="6">
+                <div class="statistic-card">
+                    <el-statistic :value="72000" title="New transactions today">
+                        <template #title>
+                            <div style="display: inline-flex; align-items: center">
+                                今日新交易
+                            </div>
+                        </template>
+                    </el-statistic>
+                    <div class="statistic-footer">
+                        <div class="footer-item">
+                            <span>与昨日对比</span>
+                            <span class="green">
+                                16%
+                                <el-icon>
+                                    <CaretTop />
+                                </el-icon>
+                            </span>
+                        </div>
+                        <div class="footer-item">
+                            <el-icon :size="14">
+                                <ArrowRight />
+                            </el-icon>
+                        </div>
+                    </div>
+                    <div class="absolute right-10 top-1/3">
+                        <el-icon size="36">
+                            <Coin />
+                        </el-icon>
+                    </div>
+                </div>
+            </el-col>
+            <el-col :span="6">
+                <div class="statistic-card">
+                    <el-statistic :value="72000" title="New transactions today">
+                        <template #title>
+                            <div style="display: inline-flex; align-items: center">
+                                订单
+                            </div>
+                        </template>
+                    </el-statistic>
+                    <div class="statistic-footer">
+                        <div class="footer-item">
+                            <span>对昨日对比</span>
+                            <span class="green">
+                                16%
+                                <el-icon>
+                                    <CaretTop />
+                                </el-icon>
+                            </span>
+                        </div>
+                        <div class="footer-item">
+                            <el-icon :size="14">
+                                <ArrowRight />
+                            </el-icon>
+                        </div>
+                    </div>
+                    <div class="absolute right-10 top-1/3">
+                        <el-icon size="36">
+                            <Tickets />
+                        </el-icon>
+                    </div>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+
 </template>
 
 <script lang="ts" setup>
-import { ref, watchEffect } from 'vue'
 import {
-    Sunny, Moon
-} from '@element-plus/icons-vue';
-import sliderMenu from '../../components/menu/sliderMenu.vue';
-const isCollapse = ref(false);
-const value1 = ref(true);
-
-watchEffect(() => {
-    if (value1.value) {
-        document.documentElement.classList.add('dark')
-    } else {
-        document.documentElement.classList.remove('dark')
-    }
-})
-const handleChangeMenu = () => {
-    // 左侧菜单隐藏展开
-    isCollapse.value = !isCollapse.value;
-}
-
-const activeIndex = ref('1')
-const handleSelect = (key: string, keyPath: string[]) => {
-    console.log(key, keyPath)
-};
-
-
-
+    ArrowRight,
+    CaretBottom,
+    CaretTop,
+    Tickets,
+    Warning,
+} from '@element-plus/icons-vue'
 </script>
 
-<style lang="scss" scoped>
-.el-menu--horizontal>.el-menu-item:nth-child(2) {
-    margin-right: auto;
+<style scoped>
+:global(h2#card-usage ~ .example .example-showcase) {
+    background-color: var(--el-fill-color) !important;
 }
 
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 200px;
-    min-height: 400px;
+.el-statistic {
+    --el-statistic-content-font-size: 28px;
+}
+
+.statistic-card {
+    height: 100%;
+    padding: 20px;
+    border-radius: 4px;
+    background-color: var(--el-bg-color-overlay);
+    @apply border relative hover:shadow-lg cursor-pointer transition-all;
+}
+
+.statistic-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    font-size: 12px;
+    color: var(--el-text-color-regular);
+    margin-top: 16px;
+}
+
+.statistic-footer .footer-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.statistic-footer .footer-item span:last-child {
+    display: inline-flex;
+    align-items: center;
+    margin-left: 4px;
+}
+
+.green {
+    color: var(--el-color-success);
+}
+
+.red {
+    color: var(--el-color-error);
 }
 </style>
